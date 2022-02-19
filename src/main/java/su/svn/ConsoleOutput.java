490c47ac9cdc6c46509282ef3f5277b43b22fa14
Victor N. Skurikhin <vskurikhin@gmail.com>
2022-02-19 09:52:57 +0300

    Функциональный тест для UnsafeSequence.

---
+++ src/main/java/su/svn/ConsoleOutput.java
@@ -0,0 +1,44 @@
+package su.svn;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public final class ConsoleOutput {
+
+    private static ConsoleOutput consoleOutput;
+
+    private final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+    private final PrintStream old;
+
+    public synchronized static ConsoleOutput redirect() {
+        if (consoleOutput == null) {
+            consoleOutput = new ConsoleOutput();
+        }
+        consoleOutput.consoleOutputRedirect();
+        return consoleOutput;
+    }
+
+    private ConsoleOutput() {
+        old = System.out;
+    }
+
+    private void consoleOutputRedirect() {
+        PrintStream ps = new PrintStream(baos);
+        System.setOut(ps);
+    }
+
+    public void revertBack() {
+        System.out.flush();
+        System.setOut(old);
+    }
+
+    public static ConsoleOutput get() {
+        return consoleOutput;
+    }
+
+    @Override
+    public String toString() {
+        return baos.toString();
+    }
+}
--- src/main/java/su/svn/Main.java
+++ src/main/java/su/svn/Main.java
@@ -8,13 +8,33 @@
 
 package su.svn;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Welcome!
  */
 public class Main {
 
+    private static void check(String consoleOutput) {
+        List<Integer> list = Arrays.stream(consoleOutput.trim().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        for (int i = 0; i <
+                UnsafeSequenceThreadExecutor.MAX_SIZE; i++) {
+            if ( ! list.contains(i)) {
+                System.out.println("Fail, result not contained: " + i);
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome!");
+        ConsoleOutput consoleOutput = ConsoleOutput.redirect();
+        UnsafeSequenceThreadExecutor.race();
+        consoleOutput.revertBack();
+        check(consoleOutput.toString());
         System.out.println("Bye!");
     }
 }

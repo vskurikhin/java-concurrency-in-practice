506fe88458d686efa068cb8a66d5e70ab7230b2c
Victor N. Skurikhin <vskurikhin@gmail.com>
2022-02-19 14:00:38 +0300

    Потокобезопасный генератор последовательности

---
+++ src/test/java/su/svn/MainTest.java
@@ -0,0 +1,10 @@
+package su.svn;
+
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+
+    public void testMethod_main() throws Exception {
+        Main.main(new String[]{});
+    }
+}

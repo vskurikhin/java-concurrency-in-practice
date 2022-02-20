66bde108983a26daf8a5093dfe04ce9d23140a37
Victor N. Skurikhin <vskurikhin@gmail.com>
2022-02-20 21:37:39 +0300

    Работа с Environment.

--- src/main/java/su/svn/enums/Environment.java
+++ src/main/java/su/svn/enums/Environment.java
@@ -1,13 +1,27 @@
 package su.svn.enums;
 
-import su.svn.utils.TomcatConfigurer;
+import java.util.Optional;
 
 public enum Environment {
     Environment;
 
-    public static final int PORT = TomcatConfigurer.port("8080");
+    public static final int PORT = port("8080");
 
-    public static final String STATIC_DIR = TomcatConfigurer.staticDir("/tmp/tomcat-static");
+    public static final String STATIC_DIR = staticDir("/tmp/tomcat-static");
 
-    public static final String TMP_DIR = TomcatConfigurer.tmpDir("/tmp/tomcat-tmp");
+    public static final String TMP_DIR = tmpDir("/tmp/tomcat-tmp");
+
+    public static int port(String port) {
+        return Optional.ofNullable(System.getenv("PORT"))
+                .map(Integer::parseInt)
+                .orElse(Integer.parseInt(port));
+    }
+
+    public static String staticDir(String dir) {
+        return Optional.ofNullable(System.getenv("STATIC_DIR")).orElse("/tmp/tomcat-static");
+    }
+
+    public static String tmpDir(String dir) {
+        return Optional.ofNullable(System.getenv("TMP_DIR")).orElse(dir);
+    }
 }

720ffa1861feb836eb75df392f607a9f8db1e2a5
Victor N. Skurikhin <vskurikhin@gmail.com>
2022-02-20 14:25:48 +0300

    Добавлен StatelessFactorizer.

--- src/main/java/su/svn/Application.java
+++ src/main/java/su/svn/Application.java
@@ -1,29 +1,13 @@
 package su.svn;
 
+import net.jcip.examples.StatelessFactorizer;
 import org.apache.catalina.LifecycleException;
 import su.svn.tomcat.Embedded;
 import su.svn.utils.SLF4JConfigurer;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 public final class Application {
 
     private final Embedded tomcat;
-
-    private final HttpServlet servlet = new HttpServlet() {
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-            PrintWriter writer = resp.getWriter();
-
-            writer.println("<html><title>Welcome</title><body>");
-            writer.println("<h1>Have a Great Day!</h1>");
-            writer.println("</body></html>");
-        }
-    };
 
     public static Application create() {
         return new Application();
@@ -34,7 +18,7 @@ public final class Application {
         SLF4JConfigurer.install();
         this.tomcat = Embedded.get();
 
-        // need for proper destroying servlets
+        // нужно для правильной остановки сервлетов
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try {
                 tomcat.stop();
@@ -45,7 +29,7 @@ public final class Application {
     }
 
     public void start() throws LifecycleException {
-        tomcat.start(servlet);
+        tomcat.start(new StatelessFactorizer());
     }
 
     public void stop() throws LifecycleException {
---
+++ src/test/java/net/jcip/examples/StatelessFactorizerTest.java
@@ -0,0 +1,29 @@
+package net.jcip.examples;
+
+import junit.framework.TestCase;
+
+public class StatelessFactorizerTest extends TestCase {
+
+    StatelessFactorizer statelessFactorizer;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        statelessFactorizer = new StatelessFactorizer();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testService() {
+
+    }
+
+    public void testEncodeIntoResponse() {
+    }
+
+    public void testExtractFromRequest() {
+    }
+
+    public void testFactor() {
+    }
+}
